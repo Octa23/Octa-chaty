b9410bdb515ab69a4f1ee9a4fff3141fdfc6401b
Octa23 <octa233@gmail.com>
2022-01-26 21:26:13 -0300

    Actualizacion en tiempo real

--- components/Chat/Post.js
+++ components/Chat/Post.js
@@ -1,11 +1,8 @@
 import {Box, Divider, Image, Stack, Text} from "@chakra-ui/react";
 
-import useTimeAgo from "../../hooks/useTimeago";
-
+import useTimeAgo from "../../hooks/useTimeAgo.js";
 const Post = ({avatar, displayName, message, createAt, messagesEndRef}) => {
-  if (createdAt) {
-    const timeago = useTimeAgo(createAt);
-  }
+  const timeago = useTimeAgo(createAt);
 
   return (
     <Box>
@@ -15,7 +12,7 @@ const Post = ({avatar, displayName, message, createAt, messagesEndRef}) => {
           <Stack alignItems={"baseline"} direction={"row"}>
             <Text>{displayName}</Text>
             <Text fontSize={13} fontWeight={"thin"}>
-              {timeago && timeago}
+              {timeago}
             </Text>
           </Stack>
           <Text fontWeight={300}>{message}</Text>
